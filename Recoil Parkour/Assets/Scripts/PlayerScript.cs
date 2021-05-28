45a231e0b1dcd0c2b401740200118e4a69672696
unknown <arafi304@gmail.com>
2021-05-28 16:10:01 +0600

    PlayerController Implemented
    Added Movement,Gravity,Jump,Camera Movement

--- Recoil Parkour/Assets/Scripts/PlayerScript.cs
+++ Recoil Parkour/Assets/Scripts/PlayerScript.cs
@@ -6,10 +6,19 @@ public class PlayerScript : MonoBehaviour
 {
     public Rigidbody playerRB;
     public CharacterController controller;
+    
 
     [Header("Movement")]
     public float speed;
-    public float jumpForce;
+    public float jumpHeight;
+    
+    public float gravity = 9.8f;
+    public Vector3 velocity;
+
+    [Header("GroundCheck")]
+    public LayerMask groundLayer;
+    public float rayLength;
+
     void Start()
     {
         
@@ -25,7 +34,38 @@ public class PlayerScript : MonoBehaviour
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
+        velocity.y += gravity * Time.deltaTime;
+
         Vector3 move = transform.right * x + transform.forward * z;
         controller.Move(move * speed * Time.deltaTime);
+        controller.Move(velocity * Time.deltaTime);
+
+        if(GroundCheck())
+        {
+            velocity.y = 0f;
+        }
+        if(Input.GetKeyDown(KeyCode.Space) && GroundCheck())
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            Debug.Log("Jump button pressed");
+        }
+    }
+
+    public bool GroundCheck()
+    {
+        RaycastHit hit;
+        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.down) * rayLength, Color.red);
+
+        if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit, rayLength, groundLayer))
+        {
+            
+            Debug.Log("Touched Ground");
+            return true;
+        }
+        else
+        {
+            Debug.Log("Didn't hit");
+            return false;
+        }
     }
 }
